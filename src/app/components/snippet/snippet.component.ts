7aa7bf6fd17659631c01ce3e00e6bebd82d07aa1
Fernando Cortes <53202695+Ferloxc@users.noreply.github.com>
2023-08-27 02:57:02 -0600

    Snip update

--- src/app/components/snippet/snippet.component.ts
+++ src/app/components/snippet/snippet.component.ts
@@ -1,40 +1,22 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-
-interface Snippet {
-  language: string;
-  code: string;
-}
-
 @Component({
   selector: 'app-snippet',
   templateUrl: './snippet.component.html',
   styleUrls: ['./snippet.component.scss']
 })
 export class SnippetComponent {
-  selectedLanguage: string = 'html';
-  snippetCode: string = '';
-  snippets: Snippet[] = [];
+  name: string;
+  code: string;
 
 
   constructor(private router: Router) {}
 
-
-  addSnippet() {
-    if (this.snippetCode) {
-      const newSnippet: Snippet = {
-        language: this.selectedLanguage,
-        code: this.snippetCode
-      };
-      this.snippets.push(newSnippet);
-      this.snippetCode = '';
-    }
-  }
   saveSnippets() {
-    // Aquí puedes implementar la lógica para guardar los snippets en algún servicio o backend
-    console.log('Guardando los snippets:', this.snippets);
-    alert('Snippets guardados exitosamente.');
+    if(this.name && this.code){
+      console.log('Guardando los snippets:', this.name, this.code);
+    }    
   }
 
   viewProjects() {
