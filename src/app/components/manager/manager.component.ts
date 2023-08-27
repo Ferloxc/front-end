ecb8cbe99785ec15b4e2dfe1387e6d8cdee48374
Fernando Cortes <53202695+Ferloxc@users.noreply.github.com>
2023-08-27 00:19:15 -0600

    Update Componente Manager y Snippet

--- src/app/components/manager/manager.component.ts
+++ src/app/components/manager/manager.component.ts
@@ -13,9 +13,24 @@ interface Project {
 export class ManagerComponent {
 
   projects: Project[] = [
-    { name: 'Project 1' },
+    { name: 'Project 1 asdasdasd' },
     { name: 'Project 2' },
-    { name: 'Project 3' }
+    { name: 'Project 3 asd asd asd a' },
+    { name: 'Project 3' },
+    { name: 'Project 3' },
+    { name: 'Project 3' },
+    { name: 'Project 3' },
+    { name: 'Project 3' },
+    { name: 'Project 1 asdasdasd' },
+    { name: 'Project 2' },
+    { name: 'Project 3' },
+    { name: 'Project 3' },
+    { name: 'Project 3' },
+    { name: 'Project 3' },
+    { name: 'Project 3' },
+    { name: 'Project 3' },
+
+
     // Add more projects here
   ];
 
--- src/app/components/snippet/snippet.component.ts
+++ src/app/components/snippet/snippet.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 interface Snippet {
   language: string;
@@ -15,15 +17,30 @@ export class SnippetComponent {
   snippetCode: string = '';
   snippets: Snippet[] = [];
 
+
+  constructor(private router: Router) {}
+
+
   addSnippet() {
     if (this.snippetCode) {
       const newSnippet: Snippet = {
         language: this.selectedLanguage,
         code: this.snippetCode
       };
-
       this.snippets.push(newSnippet);
       this.snippetCode = '';
     }
   }
+  saveSnippets() {
+    // Aquí puedes implementar la lógica para guardar los snippets en algún servicio o backend
+    console.log('Guardando los snippets:', this.snippets);
+    alert('Snippets guardados exitosamente.');
+  }
+
+  viewProjects() {
+    // Navegar a la página de proyectos guardados
+    this.router.navigate(['/manager']);
+  }
+
+
 }
